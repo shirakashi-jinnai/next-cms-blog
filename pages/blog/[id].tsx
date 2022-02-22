6f109c027f0abc39437cbfdd10bf6eb926666058
shirakashi-jinnai <j.shirakashi.jb@gmail.com>
2022-02-22 18:23:13 +0900

    categoryを追加

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -4,7 +4,12 @@ import Head from 'next/head'
 import { GetStaticPaths } from 'next'
 
 export default function BlogContent({ data }) {
-  const { title, body, publishedAt, category } = data
+  const {
+    title,
+    body,
+    publishedAt,
+    category: { name },
+  } = data
   return (
     <div>
       <Head>
@@ -17,7 +22,7 @@ export default function BlogContent({ data }) {
           __html: `${body}`,
         }}
       />
-      <p>{category}</p>
+      <p>{name}</p>
     </div>
   )
 }
