1986ac1accb101ed27165b82122510eb89131b10
shirakashi-jinnai <j.shirakashi.jb@gmail.com>
2022-02-22 17:09:09 +0900

    complete tutorial

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,12 +1,39 @@
 import React from 'react'
 import { client } from '../../libs/client'
+import Head from 'next/head'
+import { GetStaticPaths } from 'next'
 
 export default function BlogContent({ data }) {
-  console.log(data)
-  return <div>[id]</div>
+  const { title, body, publishedAt } = data
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <h1>{title}</h1>
+      <p>{publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${body}`,
+        }}
+      />
+    </div>
+  )
 }
 
-export const getServerSideProps = async (ctx) => {
+export const getStaticPaths: GetStaticPaths = async () => {
+  const data = await client.get({ endpoint: 'blog' })
+  const paths: string[] = data.contents.map(
+    //params:{id:content.id}でも可能
+    (content: any) => `/blog/${content.id}`,
+  )
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps = async (ctx) => {
   const { id } = ctx.params
   const data = await client.get({ endpoint: 'blog', contentId: id })
 
